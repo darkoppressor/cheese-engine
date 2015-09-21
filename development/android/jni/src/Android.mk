dfa116eea15dea5a227362bdadfec89aba990dbb
Kevin Wells <darkoppressor@gmail.com>
2015-09-21 01:19:34 -0500

    Add previously ignored Android makefile

---
+++ development/android/jni/src/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := Cheese-Engine
+
+SDL_PATH := ../SDL2
+SDL_IMAGE_PATH := ../SDL2_image
+SDL_MIXER_PATH := ../SDL2_mixer
+RAKNET_PATH := ../RakNet
+BOOST_PATH := ../boost
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
+$(LOCAL_PATH)/$(SDL_IMAGE_PATH) \
+$(LOCAL_PATH)/$(SDL_MIXER_PATH) \
+$(LOCAL_PATH)/$(BOOST_PATH) \
+$(LOCAL_PATH)/$(RAKNET_PATH)/..
+
+# Add your application source files here...
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/../../../../*.cpp)
+
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer RakNet
+
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
+
+include $(BUILD_SHARED_LIBRARY)
