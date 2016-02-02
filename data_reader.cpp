92102714527075786606ddcdb8f64e3886a447e5
Kevin Wells <darkoppressor@gmail.com>
2016-02-02 02:02:41 -0600

    Fix multiline comments in Data_Reader

--- data_reader.cpp
+++ data_reader.cpp
@@ -8,36 +8,59 @@
 
 using namespace std;
 
+bool Data_Reader::check_for_comment_ender(string& line){
+    if(boost::algorithm::contains(line,"*/")){
+        boost::iterator_range<string::iterator> range_comment_end=boost::algorithm::find_first(line,"*/");
+        boost::iterator_range<string::iterator> range_start_to_comment_end=boost::make_iterator_range(line.begin(),range_comment_end.end());
+        boost::algorithm::erase_range(line,range_start_to_comment_end);
+        boost::algorithm::trim(line);
+
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 vector<string> Data_Reader::read_data(File_IO_Load* load,string end_tag){
     vector<string> lines;
 
     bool multi_line_comment=false;
+    bool early_break=false;
 
-    while(!load->eof()){
+    while(!early_break && !load->eof()){
         string line="";
 
         load->getline(&line);
         boost::algorithm::trim(line);
 
-        if(boost::algorithm::contains(line,"*/")){
-            multi_line_comment=false;
-        }
-
-        if(!multi_line_comment){
-            if(boost::algorithm::starts_with(line,"/*")){
-                multi_line_comment=true;
-
-                continue;
+        while(true){
+            if(multi_line_comment && check_for_comment_ender(line)){
+                multi_line_comment=false;
             }
-            else if(boost::algorithm::starts_with(line,"//")){
-            }
-            else if(boost::algorithm::starts_with(line,end_tag)){
-                lines.push_back(line);
 
-                break;
+            if(!multi_line_comment){
+                if(boost::algorithm::starts_with(line,"/*")){
+                    multi_line_comment=true;
+                }
+                else if(boost::algorithm::starts_with(line,"//")){
+                    break;
+                }
+                else if(boost::algorithm::starts_with(line,end_tag)){
+                    lines.push_back(line);
+
+                    early_break=true;
+
+                    break;
+                }
+                else{
+                    lines.push_back(line);
+
+                    break;
+                }
             }
             else{
-                lines.push_back(line);
+                break;
             }
         }
     }
--- data_reader.h
+++ data_reader.h
@@ -11,6 +11,13 @@
 #include <string>
 
 class Data_Reader{
+private:
+
+    //Returns true if a comment ender was found
+    //Returns false if no comment ender was found
+    //If a comment ender is found, the passed string is erased up to and including the comment ender
+    static bool check_for_comment_ender(std::string& line);
+
 public:
 
     //Reads data from the passed file one line at a time until reaching either the end of the file or a line beginning with the passed end tag
