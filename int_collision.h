2a05e1070f22ceec56dea6391be0787562179d15
Kevin Wells <darkoppressor@gmail.com>
2016-02-02 02:51:23 -0600

    Bring Int_ classes in from hestia
    Classes brought in: Int_Math, Int_Collision, Int_Vector_Components, and
    Int_Vector

---
+++ int_collision.h
@@ -0,0 +1,33 @@
+/* Copyright (c) 2012 Cheese and Bacon Games, LLC */
+/* This file is licensed under the MIT License. */
+/* See the file docs/LICENSE.txt for the full license text. */
+
+#ifndef int_collision_h
+#define int_collision_h
+
+#include "collision.h"
+#include "int_math.h"
+
+#include <cstdint>
+
+class Int_Collision{
+public:
+
+    static std::int32_t get_angle_to_rect(const Collision_Rect<std::int32_t>& box_a,const Collision_Rect<std::int32_t>& box_b){
+        return Int_Math::get_angle_to_point(box_a.get_center(),box_b.get_center());
+    }
+
+    static std::int32_t get_angle_to_rect(const Collision_Circ<std::int32_t>& circle,const Collision_Rect<std::int32_t>& box){
+        return Int_Math::get_angle_to_point(circle.get_center(),box.get_center());
+    }
+
+    static std::int32_t get_angle_to_circ(const Collision_Circ<std::int32_t>& circle_a,const Collision_Circ<std::int32_t>& circle_b){
+        return Int_Math::get_angle_to_point(circle_a.get_center(),circle_b.get_center());
+    }
+
+    static std::int32_t get_angle_to_circ(const Collision_Rect<std::int32_t>& box,const Collision_Circ<std::int32_t>& circle){
+        return Int_Math::get_angle_to_point(box.get_center(),circle.get_center());
+    }
+};
+
+#endif
---
+++ int_vector.h
@@ -0,0 +1,62 @@
+/* Copyright (c) 2012 Cheese and Bacon Games, LLC */
+/* This file is licensed under the MIT License. */
+/* See the file docs/LICENSE.txt for the full license text. */
+
+#ifndef int_vector_h
+#define int_vector_h
+
+#include <cstdint>
+
+class Int_Vector_Components{
+public:
+
+    std::int32_t a;
+    std::int32_t b;
+
+    Int_Vector_Components();
+    Int_Vector_Components(std::int32_t get_a,std::int32_t get_b);
+
+    Int_Vector_Components operator+(const Int_Vector_Components& vc1) const;
+    void operator+=(const Int_Vector_Components& vc1);
+
+    Int_Vector_Components operator-(const Int_Vector_Components& vc1) const;
+    void operator-=(const Int_Vector_Components& vc1);
+
+    Int_Vector_Components operator*(std::int32_t scalar) const;
+    void operator*=(std::int32_t scalar);
+
+    Int_Vector_Components operator/(std::int32_t scalar) const;
+    void operator/=(std::int32_t scalar);
+};
+
+class Int_Vector{
+public:
+
+    std::int32_t magnitude;
+    std::int32_t direction;
+
+    Int_Vector();
+    Int_Vector(std::int32_t get_magnitude,std::int32_t get_direction);
+    Int_Vector(const Int_Vector_Components& vc);
+
+    Int_Vector_Components get_components() const;
+    Int_Vector_Components get_components_absolute() const;
+    void set_polar_form(const Int_Vector_Components& vc);
+    Int_Vector opposite() const;
+
+    Int_Vector operator+(const Int_Vector& vector1) const;
+    void operator+=(const Int_Vector& vector1);
+
+    Int_Vector operator-(const Int_Vector& vector1) const;
+    void operator-=(const Int_Vector& vector1);
+
+    Int_Vector operator*(std::int32_t scalar) const;
+    void operator*=(std::int32_t scalar);
+
+    Int_Vector operator/(std::int32_t scalar) const;
+    void operator/=(std::int32_t scalar);
+
+    std::int32_t dot_product(const Int_Vector& vector1) const;
+};
+
+#endif
