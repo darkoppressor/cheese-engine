dc6cb7e199672d6b6533cfbb1c273550ab5af997
Kevin Wells <darkoppressor@gmail.com>
2016-02-04 23:53:02 -0600

    Improve error handling in Android method calling

--- android.h
+++ android.h
@@ -10,6 +10,8 @@
 #include <cstdint>
 
 #ifdef GAME_OS_ANDROID
+    #include "log.h"
+
     #include <SDL.h>
     #include <jni.h>
 #endif
@@ -81,12 +83,24 @@ private:
     static void call_android_method(std::string method_name,std::string signature,Args... args){
         #ifdef GAME_OS_ANDROID
             JNIEnv* env=(JNIEnv*)SDL_AndroidGetJNIEnv();
-            jobject activity=(jobject)SDL_AndroidGetActivity();
-            jclass clazz(env->GetObjectClass(activity));
-            jmethodID method_id=env->GetStaticMethodID(clazz,method_name.c_str(),signature.c_str());
-            env->CallStaticVoidMethod(clazz,method_id,args...);
-            env->DeleteLocalRef(activity);
-            env->DeleteLocalRef(clazz);
+
+            if(env!=0){
+                jobject activity=(jobject)SDL_AndroidGetActivity();
+
+                if(activity!=NULL){
+                    jclass clazz(env->GetObjectClass(activity));
+                    jmethodID method_id=env->GetStaticMethodID(clazz,method_name.c_str(),signature.c_str());
+                    env->CallStaticVoidMethod(clazz,method_id,args...);
+                    env->DeleteLocalRef(activity);
+                    env->DeleteLocalRef(clazz);
+                }
+                else{
+                    Log::add_error("Error calling Android method '"+method_name+"': SDL_AndroidGetActivity returned NULL");
+                }
+            }
+            else{
+                Log::add_error("Error calling Android method '"+method_name+"': SDL_AndroidGetJNIEnv returned 0");
+            }
         #endif
     }
 
