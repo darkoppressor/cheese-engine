b8491bdfa78498b541226062ccc45d4e88b6e83c
Kevin Wells <darkoppressor@gmail.com>
2016-08-14 18:39:50 -0500

    Update engine version

--- engine_version.cpp
+++ engine_version.cpp
@@ -16,11 +16,11 @@ string Engine_Version::get_engine_status(){
 }
 
 string Engine_Version::get_engine_version(){
-    return "2.1.0";
+    return "2.1.1";
 }
 
 string Engine_Version::get_engine_date(){
-    return "2016-08-11";
+    return "2016-08-14";
 }
 
 Version_Series::Version_Series(string get_first_version,string get_last_version){
