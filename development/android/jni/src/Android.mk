cc4ca267c5b8ee93156cadbbdeb6b1e1d08432be
Kevin Wells <darkoppressor@gmail.com>
2016-08-17 06:38:06 -0500

    Include Google Play Games SDK

--- development/android/jni/src/Android.mk
+++ development/android/jni/src/Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE := Cheese-Engine
 
 LOCAL_MODULE_FILENAME := libCheese-Engine
 
+GPG_PATH := ../gpg
 SDL_PATH := ../SDL2
 SDL_IMAGE_PATH := ../SDL2_image
 SDL_MIXER_PATH := ../SDL2_mixer
@@ -16,12 +17,13 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
 $(LOCAL_PATH)/$(SDL_IMAGE_PATH) \
 $(LOCAL_PATH)/$(SDL_MIXER_PATH) \
 $(LOCAL_PATH)/$(BOOST_PATH) \
+$(LOCAL_PATH)/$(GPG_PATH)/include \
 $(LOCAL_PATH)/$(RAKNET_PATH)/..
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/../../../../*.cpp)
 
-LOCAL_STATIC_LIBRARIES := SDL2_static SDL2_image_static SDL2_mixer_static RakNet_static
+LOCAL_STATIC_LIBRARIES := libgpg-1 SDL2_static SDL2_image_static SDL2_mixer_static RakNet_static
 
 LOCAL_EXPORT_LDLIBS := -lGLESv1_CM -lGLESv2 -llog -lz -landroid
 
