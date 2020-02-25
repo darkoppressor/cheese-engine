8856d36bc5f6c313da0a81e58d2a2311c3dc6cff
Kevin Wells <darkoppressor@gmail.com>
2020-02-25 00:49:35 -0500

    Fix CI/CD
    Signed-off-by: Kevin Wells <darkoppressor@gmail.com>

--- ci-cd.py
+++ ci-cd.py
@@ -1,8 +1,8 @@
 import sys
 import argparse
 import logging
-import subprocess
 import os
+import subprocess
 
 sys.path.append('/home/tails/server/feeds/')
 import feeds
@@ -25,32 +25,28 @@ def main(argv):
     ciBuildUrl = args.ciBuildUrl
 
     try:
-        text = 'Build started - ' + ciJobName + ' ' + ciBuildNumber + ' (' + ciBuildUrl + ')'
+        text = 'Build started - ' + ciJobName + ' ' + ciBuildNumber + ' (<a href="' + ciBuildUrl + '">Open</a>)'
         feeds.post('jenkins', 'Wells Family Jenkins', 'Tails', 'tails.at.mobius@gmail.com', 'https://wells-family.xyz/', 'https://wells-family.xyz/favicon.png', text, text)
 
-        logging.warning('CWD: ' + os.getcwd())
-
-        subprocess.run(['chmod', '+x', 'tools/*'])
-        subprocess.run(['chmod', '+x', 'tools/build-system/*'])
-        subprocess.run(['chmod', '+x', 'tools/build-system/scripts/*'])
-        subprocess.run(['chmod', '-x', 'tools/build-system/scripts/*-x86_64'])
-        subprocess.run(['chmod', '-x', 'tools/build-system/scripts/*.cmake'])
-        subprocess.run(['chmod', '+x', 'tools/build-system/scripts/android/*'])
-        subprocess.run(['chmod', '+x', 'development/android/android-prep'])
-        subprocess.run(['chmod', '+x', 'development/android/clean-android'])
+        os.chmod('tools/*', 0o755)
+        os.chmod('tools/build-system/*', 0o755)
+        os.chmod('tools/build-system/scripts/*', 0o755)
+        os.chmod('tools/build-system/scripts/android/*', 0o755)
+        os.chmod('development/android/android-prep', 0o755)
+        os.chmod('development/android/clean-android', 0o755)
 
         if subprocess.run(['tools/build-system/scripts/build-engine', '$(pwd)']):
-            text = 'Build completed successfully - ' + ciJobName + ' ' + ciBuildNumber + ' (' + ciBuildUrl + ')'
+            text = 'Build completed successfully - ' + ciJobName + ' ' + ciBuildNumber + ' (<a href="' + ciBuildUrl + '">Open</a>)'
             feeds.post('jenkins', 'Wells Family Jenkins', 'Tails', 'tails.at.mobius@gmail.com', 'https://wells-family.xyz/', 'https://wells-family.xyz/favicon.png', text, text)
 
             if subprocess.run(['tools/build-system/scripts/deploy-engine', '$(pwd)']):
-                text = 'Deployment completed successfully - ' + ciJobName + ' ' + ciBuildNumber + ' (' + ciBuildUrl + ')'
+                text = 'Deployment completed successfully - ' + ciJobName + ' ' + ciBuildNumber + ' (<a href="' + ciBuildUrl + '">Open</a>)'
                 feeds.post('jenkins', 'Wells Family Jenkins', 'Tails', 'tails.at.mobius@gmail.com', 'https://wells-family.xyz/', 'https://wells-family.xyz/favicon.png', text, text)
             else:
-                text = 'Deployment failed - ' + ciJobName + ' ' + ciBuildNumber + ' (' + ciBuildUrl + ')'
+                text = 'Deployment failed - ' + ciJobName + ' ' + ciBuildNumber + ' (<a href="' + ciBuildUrl + '">Open</a>)'
                 feeds.post('jenkins', 'Wells Family Jenkins', 'Tails', 'tails.at.mobius@gmail.com', 'https://wells-family.xyz/', 'https://wells-family.xyz/favicon.png', text, text)
         else:
-            text = 'Build failed - ' + ciJobName + ' ' + ciBuildNumber + ' (' + ciBuildUrl + ')'
+            text = 'Build failed - ' + ciJobName + ' ' + ciBuildNumber + ' (<a href="' + ciBuildUrl + '">Open</a>)'
             feeds.post('jenkins', 'Wells Family Jenkins', 'Tails', 'tails.at.mobius@gmail.com', 'https://wells-family.xyz/', 'https://wells-family.xyz/favicon.png', text, text)
 
     except Exception as e:
