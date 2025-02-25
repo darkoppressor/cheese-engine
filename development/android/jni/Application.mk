962952dbd7f458c725366b821c5a26a620eafd2a
Kevin Wells <darkoppressor@gmail.com>
2025-02-25 00:42:34 -0500

    Specify NDK version and update Android API level
    Signed-off-by: Kevin Wells <darkoppressor@gmail.com>

--- development/android/jni/Application.mk
+++ development/android/jni/Application.mk
@@ -8,7 +8,7 @@ NDK_TOOLCHAIN_VERSION := clang
 APP_ABI := arm64-v8a x86_64
 
 # Minimum runtime API level
-APP_PLATFORM=android-31
+APP_PLATFORM=android-34
 
 # Release options:
 APP_OPTIM := release
